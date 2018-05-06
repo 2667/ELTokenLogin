52b5ff2cb297d7faeeeb5da9ce096d84f72eca31
唐斌 <2667860931@qq.com>
2018-05-06 18:12:09 +0800

    no message

--- ELTokenLogin/ELTokenLogin/ViewController.m
+++ ELTokenLogin/ELTokenLogin/ViewController.m
@@ -22,11 +22,13 @@
     UIButton *weixin = [[UIButton alloc] initWithFrame:CGRectMake(100, 80, 100, 60)];
     weixin.backgroundColor = [UIColor redColor];
     weixin.titleLabel.text = @"weixin";
+    [weixin setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [weixin addTarget:self action:@selector(weixin) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:weixin];
     // Do any additional setup after loading the view, typically from a nib.
     UIButton *qq = [[UIButton alloc] initWithFrame:CGRectMake(100, 150, 100, 60)];
     qq.backgroundColor = [UIColor redColor];
+     [qq setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     qq.titleLabel.text = @"qq";
     [qq addTarget:self action:@selector(qq) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:qq];
@@ -34,6 +36,8 @@
     UIButton *sina = [[UIButton alloc] initWithFrame:CGRectMake(100, 240, 100, 60)];
     sina.backgroundColor = [UIColor redColor];
     sina.titleLabel.text = @"sina";
+//     [sina setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    sina.titleLabel.textColor = [UIColor blueColor];
     [sina addTarget:self action:@selector(sina) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:sina];
     
